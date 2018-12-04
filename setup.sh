0865deb9fb05319646ad58213a260831d983c041
Dev <31535748+okpluscoin@users.noreply.github.com>
2018-12-04 03:23:16 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,7 +30,18 @@ tail -f /home/scribeuser/01vpsmnlog.txt
 
 
 
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
 
+sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils software-properties-common libgmp3-dev -y
+sudo apt-get install libboost-all-dev -y
+sudo add-apt-repository ppa:bitcoin/bitcoin -y
+sudo apt-get update -y
+sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
+sudo apt-get install libminiupnpc-dev -y
+sudo apt-get -y install python-virtualenv virtualenv
+sudo apt-get install nano jq htop git pwgen libzmq3-dev bc -y
 
 
 
