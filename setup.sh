708b00988aed4f643df26f7dbb473e7166e30eac
Dev <31535748+okpluscoin@users.noreply.github.com>
2018-12-04 02:47:39 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+printf "
+
+#############################
+#    Welcome to your new    #
+#     Scribe Masternode     #
+#############################
+
+The install has started, follow the progress by
+typing the following on the command line:
+
+tail -f /home/scribeuser/01vpsmnlog.txt
+
+" > ~/MASTERNODE_INSTALL_IN_PROGRESS
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+useradd -m -s /bin/bash scribeuser
+
+
+
+su - scribeuser bash -c 'wget -O 01vpsscribe.sh http://dl.madstu.net/01vpsscribe.sh && sh 01vpsscribe.sh'
+
+printf "
+
+#############################
+#    Welcome to your new    #
+#     01Coin Masternode     #
+#############################
+
+The install has finished, find your required masternode key
+in the file located here:
+
+/home/scribeuser/01vpsmnlog.txt
+
+" > ~/MASTERNODE_INSTALL_COMPLETE
+rm ~/MASTERNODE_INSTALL_IN_PROGRESS
