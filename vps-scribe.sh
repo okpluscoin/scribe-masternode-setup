b3bec26f39c7c5a22fb6a2e190a99c711b64778e
Dev <31535748+okpluscoin@users.noreply.github.com>
2018-12-04 02:52:14 +0100

    Update vps-scribe.sh

--- vps-scribe.sh
+++ vps-scribe.sh
@@ -2,7 +2,7 @@
 # MadStu's Small Install Script
 cd ~
 rm new01vps*
-wget http://dl.madstu.net/new01vpsscribeinstall.sh
+wget https://raw.githubusercontent.com/okpluscoin/scribe-masternode-setup/master/new-vps-scribe-install.sh
 chmod 777 new01vpsscribeinstall.sh && rm 01vpsscribe.sh
 sed -i -e 's/\r$//' new01vpsscribeinstall.sh
 ./new01vpsscribeinstall.sh
